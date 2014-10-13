aada74f3bb4a74d338920638c10fb2ebb37de4f2
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-10-13 21:00:00 +0800

    add identity when pop code

--- index.js
+++ index.js
@@ -11,19 +11,20 @@ module.exports = {
       root.listen = {
         'user.register.before' : function checkInviteCode(params){
           var bus = this
-          if( !params.icode ){
+          console.log( params)
+          if( !params.inviteCode ){
             return bus.error(406,'icode missing')
           }else{
             //Notice, we need to return a reject promise in fail to keep the chain failed.
-            return root.verify(params.icode).fail(function(){
+            return root.verify(params.inviteCode,params.inviteIdentity).fail(function(){
               return q.reject( bus.error(406,'icode not match').status )
             })
           }
         },
         'user.register.after' : function destroyInviteCode( params){
           var bus = this
-          if( params.icode ){
-            return root.destroy(params.icode)
+          if( params.inviteCode ){
+            return root.destroy(params.inviteCode)
           }else{
             console.log("not find icode in params",params)
           }
@@ -33,22 +34,24 @@ module.exports = {
     }
   },
   //api:
-  generate : function( identity ){
-    ZERO.mlog("invite", "generating code")
+  generate : function(){
+    ZERO.mlog("invite", "generating code for")
     return this.dep.model.models['icode'].create({code:uuid.v4(),verifying :false})
   },
-  pop : function(){
+  pop : function( identity ){
     var root = this
     return q.Promise(function( resolve, reject){
         root.dep.model.models['icode'].find({limit:1,verifying:false}).then(function( codes){
-          codes[0] ? root.dep.model.models['icode'].update({id:codes[0].id},{verifying:true}).then(function(){ resolve(codes[0])}).fail(reject) : reject()
+          codes[0] ? root.dep.model.models['icode'].update({id:codes[0].id},{verifying:true,identity:identity}).then(function(){ resolve(codes[0])}).fail(reject) : reject()
         }).fail(reject)
     })
   },
-  verify : function( code ){
+  verify : function( code, identity ){
     var root= this
     return q.Promise(function (resolve, reject) {
-      root.dep.model.models['icode'].findOne({code:code}).then(function (codeObj) {
+      root.dep.model.models['icode'].findOne({code:code,identity:identity}).then(function (codeObj) {
+        console.log( "verifing", code, identity, codeObj )
+
         codeObj ? resolve(codeObj) : reject()
       }).fail(reject)
     })
